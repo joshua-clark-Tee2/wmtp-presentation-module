03b8137a0003a1c450108f9f94ab361cfba2080c
Joshua Clark <joshua.clark@tee2.org>
2018-12-20 11:57:10 -0800

    removed dist folder from git

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function helloWorld() {
-    var helloWorld = console.log("Hello World Typescript module");
-    return helloWorld;
-}
-exports.helloWorld = helloWorld;
